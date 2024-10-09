86263f34690a3670de556597f47a2ac72abd331e
mvoidets <mvoidets@yahoo.com>
2024-10-09 11:29:08 -0700

    made changes to my motorbike.ts

--- dist/classes/Motorbike.js
+++ dist/classes/Motorbike.js
@@ -23,9 +23,9 @@ class Motorbike extends Vehicle {
     }
     // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-    //  if (wheelie(vehicle: Motorbike): void {
-    //     console.log("Motorbike ${vehicle.make} ${vehicle.model }is doing a wheelie!")
-    //  }
+    wheelie() {
+        console.log(`Motorbike ${this.make} ${this.model}is doing a wheelie!`);
+    }
     // TODO: Override the printDetails method from the Vehicle class
     printDetails() {
         // Call the printDetails method of the parent class, Vehicle
--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -42,11 +42,11 @@ constructor(
 } 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-  if (wheelie(vehicle: Motorbike): void {
-        if (this.wheels) <= 2 {
 
-      console.log("Motorbike ${vehicle.make} ${vehicle.model }is doing a wheelie!")
-  }}
+      wheelie():void {
+
+      console.log(`Motorbike ${this.make} ${this.model }is doing a wheelie!`)
+  }
   
     // TODO: Override the printDetails method from the Vehicle class
   override printDetails(): void {
